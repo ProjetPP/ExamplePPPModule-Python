baba6e1c4bcab6e6d09976393c0a05cfdb680fdf
Valentin Lorentz <progval@progval.net>
2014-10-18 15:36:13 +0200

    Initial import of generated plugin.

---
+++ Makefile
@@ -0,0 +1,18 @@
+PYTHON=python3
+
+all:
+	$(PYTHON) setup.py
+
+install:
+	$(PYTHON) setup.py install
+
+localinstall:
+	$(PYTHON) setup.py install --user
+
+run:
+	gunicorn example_ppp_module:app
+
+tests:
+	$(PYTHON) run_tests.py
+
+.PHONY: all install localinstall tests
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='example_ppp_module',
+    version='0.1',
+    description='Example python module for the PPP.',
+    url='https://github.com/ProjetPP',
+    author='Valentin Lorentz',
+    author_email='valentin.lorentz+ppp@ens-lyon.org',
+    license='MIT',
+    classifiers=[
+        'Environment :: No Input/Output (Daemon)',
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Software Development :: Libraries',
+    ],
+    install_requires=[
+        'ppp_datamodel',
+        'ppp_core',
+    ],
+    packages=[
+        'example_ppp_module',
+    ],
+)
