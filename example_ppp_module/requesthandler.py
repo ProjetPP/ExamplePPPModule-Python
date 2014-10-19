0f7630ed68a85b2431de90cf99125ad6e5588519
Valentin Lorentz <progval@progval.net>
2014-10-19 16:26:19 +0200

    Implement the request handler.

--- example_ppp_module/requesthandler.py
+++ example_ppp_module/requesthandler.py
@@ -1,12 +1,28 @@
 """Router of the module."""
 
+import random
+
+from ppp_datamodel import Triple, Resource, Missing
+from ppp_datamodel.communication import Response
+
 from ppp_core.exceptions import ClientError
 
 class RequestHandler:
     def __init__(self, request):
-        # TODO: Implement this
-        pass
+        assert request.language == 'en'
+        self._request = request.tree
 
     def answer(self):
-        # TODO: Implement this
-        pass
+        if self._request == Triple(predicate=Resource(value='be'),
+                                   subject=Resource(value='you'),
+                                   object=Missing()):
+            # If the question has the form “You are ?.”
+            # (ie. what the NLP should give from “What are you?”)
+            answer = random.choice(['bot', 'computer', 'flower'])
+            r =  Response('en', 0.1,
+                          Triple(predicate=Resource(value='be'),
+                                 subject=Resource(value='I'),
+                                 object=Resource(value=answer)))
+            return [r]
+        else:
+            return []
